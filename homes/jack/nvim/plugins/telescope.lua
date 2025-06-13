f53b8a6c0d76fc9f05b9cb0dd66c0f96505e6e45
jack-avery <47289484+jack-avery@users.noreply.github.com>
2025-06-12 20:01:44 -0400

    add telescope

---
+++ homes/jack/nvim/plugins/telescope.lua
@@ -0,0 +1,9 @@
+require("telescope").setup({})
+
+vim.api.nvim_set_keymap("n", "<leader>f", ":Telescope find_files<cr>", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>g", ":Telescope live_grep<cr>", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>b", ":Telescope buffers<cr>", {noremap=true})
+
+return {
+  { "nvim-telescope/telescope.nvim" }
+}
