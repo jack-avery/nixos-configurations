96815691989a90dafecd3fe35818baa06808385c
jack-avery <47289484+jack-avery@users.noreply.github.com>
2025-07-30 16:56:38 -0400

    change ps1

--- homes/modules/programs/bash.nix
+++ homes/modules/programs/bash.nix
@@ -8,34 +8,55 @@
     enableCompletion = false;
 
     bashrcExtra = ''
+      C_BLACK='\033[0;30m'
+      C_RED='\033[0;31m'
+      C_GREEN='\033[0;32m'
+      C_GREENB='\033[1;32m'
+      C_YELLOW='\033[0;33m'
+      C_BLUE='\033[0;34m'
+      C_BLUEB='\033[1;34m'
+      C_MAGENTA='\033[0;35m'
+      C_MAGENTAB='\033[1;35m'
+      C_CYAN='\033[0;36m'
+      C_GRAY='\033[0;90m'
+      C_WHITE='\033[0;97m'
+      C_LBLUE='\033[0;94m'
+      C_LCYAN='\033[0;96m'
+      CB_BLUE='\033[44m'
+      CB_CYAN='\033[46m'
+      CB_LBLUE='\033[104m'
+      CB_LCYAN='\033[106m'
+      CB_WHITE='\033[107m'
+      C_END='\033[0m'
+
       if [ -f '~/git/google-cloud-sdk/path.bash.inc' ]; then . '~/git/google-cloud-sdk/path.bash.inc'; fi
       if [ -f '~/git/google-cloud-sdk/completion.bash.inc' ]; then . '~/git/google-cloud-sdk/completion.bash.inc'; fi
       eval "$(~/.rbenv/bin/rbenv init - bash 2> /dev/null)"
 
       rc_color() {
           if [[ ! $? == 0 ]] then
-              echo -e '\033[0;31m'
+              echo -e "''${C_RED}"
           else
-              echo -e '\033[0m'
+              echo -e "''${C_BLUEB}"
           fi
       }
 
-      git_ps1() {
-          local git_status="$(git status 2> /dev/null)"
-          local on_branch="On branch ([^$IFS]*)"
-          local on_commit="HEAD detached at ([^$IFS]*)"
+      # git_ps1() {
+      #     local git_status="$(git status 2> /dev/null)"
+      #     local on_branch="On branch ([^$IFS]*)"
+      #     local on_commit="HEAD detached at ([^$IFS]*)"
 
-          if [[ $git_status =~ $on_branch ]]; then
-              local branch=''${BASH_REMATCH[1]}
-              echo -e " ($branch)"
-          elif [[ $git_status =~ $on_commit ]]; then
-              local commit=''${BASH_REMATCH[1]}
-              echo -e " ($commit)"
-          fi
-      }
+      #     if [[ $git_status =~ $on_branch ]]; then
+      #         local branch=''${BASH_REMATCH[1]}
+      #         echo -e " ($branch)"
+      #     elif [[ $git_status =~ $on_commit ]]; then
+      #         local commit=''${BASH_REMATCH[1]}
+      #         echo -e " ($commit)"
+      #     fi
+      # }
 
-      PROMPT_COMMAND='RC_COLOR=$(rc_color);GITPS1=$(git_ps1)'
-      PS1=' \001\033[0;33m\002\u\001\033[0m\002@\001\033[0;33m\002\h \W\001\033[0;35m\002''${GITPS1} \001''${RC_COLOR}\002\$ \001\033[0m\002'
+      PROMPT_COMMAND='RC_COLOR=$(rc_color)'
+      PS1='\n \001\033[0;90m\002\W \001''${RC_COLOR}\002λ \001\033[0m\002'
     '';
 
     shellAliases = {
